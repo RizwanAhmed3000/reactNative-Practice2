abafaadc7d7e795ff657b8370ae5505364c5db89
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-12-16 23:43:19 +0500

    still not working

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import Onboarding2 from './Screens/Onboarding2';
 import Onboarding3 from './Screens/Onboarding3';
 import Home from './Screens/Home';
 // import MyDrawer from './navigation/Drawer.js';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 // import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
@@ -18,27 +19,26 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      {/* <GestureHandlerRootView style={{ flex: 1 }}>
-        <Ca />
-      </GestureHandlerRootView> */}
-      <NavigationContainer>
-        <SafeAreaView style={{ flex: 1 }}>
-          <StatusBar style="auto" />
-          <Stack.Navigator initialRouteName='Home' screenOptions={{
-            animation: "slide_from_right",
-            headerShown: false
-          }}  >
-            <Stack.Screen name='Splash' component={Splash} />
-            <Stack.Screen name='Onboarding' component={Onboarding} options={{ headerShown: false, headerLeft: () => null }} />
-            <Stack.Screen name='Onboarding2' component={Onboarding2} options={{ headerShown: false, headerLeft: () => null }} />
-            <Stack.Screen name='Onboarding3' component={Onboarding3} options={{ headerShown: false, headerLeft: () => null }} />
-            <Stack.Screen name='SignUp' component={SignUp} options={{ headerShown: true, title: "Sign Up", headerBackVisible: false }} />
-            <Stack.Screen name='Login' component={Login} options={{ headerShown: true, title: "Login In" }} />
-            <Stack.Screen name='Home' component={Home} options={{ headerShown: false, title: "Home" }} />
-          </Stack.Navigator>
-          {/* <MyDrawer /> */}
-        </SafeAreaView>
-      </NavigationContainer >
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar style="auto" />
+            <Stack.Navigator initialRouteName='Home' screenOptions={{
+              animation: "slide_from_right",
+              headerShown: false
+            }}  >
+              <Stack.Screen name='Splash' component={Splash} />
+              <Stack.Screen name='Onboarding' component={Onboarding} options={{ headerShown: false, headerLeft: () => null }} />
+              <Stack.Screen name='Onboarding2' component={Onboarding2} options={{ headerShown: false, headerLeft: () => null }} />
+              <Stack.Screen name='Onboarding3' component={Onboarding3} options={{ headerShown: false, headerLeft: () => null }} />
+              <Stack.Screen name='SignUp' component={SignUp} options={{ headerShown: true, title: "Sign Up", headerBackVisible: false }} />
+              <Stack.Screen name='Login' component={Login} options={{ headerShown: true, title: "Login In" }} />
+              <Stack.Screen name='Home' component={Home} options={{ headerShown: false, title: "Home" }} />
+            </Stack.Navigator>
+            {/* <MyDrawer /> */}
+          </SafeAreaView>
+        </NavigationContainer >
+      </GestureHandlerRootView>
     </>
   );
 }
--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -1,12 +1,12 @@
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 // import Article from '../Screens/Article';
 // import Home from '../Screens/Home';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
 
 // const Drawer = createDrawerNavigator();
 
 // function MyDrawer() {
 //     return (
-//         <Drawer.Navigator>
+//         <Drawer.Navigator initialRouteName="Home">
 //             <Drawer.Screen name="Home" component={Home} />
 //             <Drawer.Screen name="Article" component={Article} />
 //         </Drawer.Navigator>
